ac22c61c913e80422596d8aa7e946bf3bd42ce69
Steffen Roller <steffen.roller@gmail.com>
2016-11-11 00:10:52 -0500

    everything green

--- lib/grin.rb
+++ lib/grin.rb
@@ -6,31 +6,81 @@ require 'awesome_print'
 
 module GRIN
 
+Waterflow = {
+  westmontrose: '8725042',
+  bridgeport: '8665042',
+  victoria: '8899042',
+  doon: '8677042',
+  galt: '8671042',
+  newhamburg: '8827042'
+}
+
+Summerflow = {
+  westmontrose: 5.0,
+  bridgeport: 11.0,
+  doon: 11.0,
+  galt: 15,
+  newhamburg: 1.5
+}
+
+
 BASE_URL = 'http://kiwis.grandriver.ca/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0&format=json&request='
 
-  def self.get_station_list
+  def self.station_list
     clnt = HTTPClient.new
     r = clnt.get(BASE_URL+'getStationList')
 
     { status: r.status, content: JSON.parse(r.body)}
   end
 
-  def self.get_station_by_name(name='*')
+  def self.station_by_name(name='*')
     clnt = HTTPClient.new
     r = clnt.get(BASE_URL+"getStationList&station_name=#{name}")
     { status: r.status, content: JSON.parse((r.body))}
   end
 
-	def self.get_station_id_by_name(name)
+	def self.station_id_by_name(name)
 		return nil if name.nil?
     clnt = HTTPClient.new
     r = clnt.get(BASE_URL+"getStationList&station_name=#{name}")
 		JSON.parse(r.body)[1][2]
 	end
 
-	def self.get_timeseries_list(name)
+	def self.timeseries_list(ts_id, from, to=from)
+    return nil if ts_id.nil? || from.nil? || to.nil?
+  
 		clnt = HTTPClient.new
-    r = clnt.get(BASE_URL+"getTimeseriesList&station_name=#{name}")
-
+    r = clnt.get(BASE_URL+"gettimeseriesvalues&ts_id=#{ts_id}&from=#{from}&to=#{to}")
+    JSON.parse(r.body)
 	end
+
+  # get list of parameters for a giving station
+  def self.parameter_list(station_id)
+    clnt = HTTPClient.new
+    r = clnt.get(BASE_URL+"getParameterList&station_id=#{station_id}")
+    # ap JSON.parse(r.body)
+    JSON.parse(r.body)
+  end
+
+  def self.waterflow(station, day=nil)
+    return nil unless Waterflow.has_key? station
+    url = BASE_URL+"gettimeseriesvalues&ts_id=#{Waterflow[station]}"
+    url += "&from=#{day}T14:00:00&to=#{day}T22:00:00" unless day.nil?
+    clnt = HTTPClient.new
+    # ap url
+    r = clnt.get(url)
+    data = JSON.parse(r.body)[0]['data']
+    avg = data.inject(0.0) { |sum,item| sum += item[1] } / data.size
+    data.inject(0.0) { |sum,item| sum += item[1] } / data.size
+  end
+
+  # flow relative to normal Summer low flow
+  # 1.0 would be normal
+  # 2.0 would be double
+  # 0.5 would be half and so on
+  def self.relative_flow(station, day=nil)
+    return nil unless Summerflow.has_key? station
+    flow = waterflow(station, day)
+    flow / Summerflow[station]
+  end
 end
--- spec/lib/grin_spec.rb
+++ spec/lib/grin_spec.rb
@@ -3,27 +3,54 @@ require_relative '../spec_helper'
 describe 'Grand River Information Network' do
 
   it 'returns 200' do
-    r = GRIN.get_station_list
+    r = GRIN.station_list
     expect(r[:status]).to be(200)
   end
 
   it 'returns an Array as payload' do
-    r = GRIN.get_station_list
+    r = GRIN.station_list
     expect(r[:content]).to be_a(Array)
   end
 
   it 'gets a station by name' do
-    r = GRIN.get_station_by_name('St. Jacobs')
+    r = GRIN.station_by_name('St. Jacobs')
     expect(r[:content][1][0]).to eq('St. Jacobs')
   end
 
   it 'gets the id for a given name' do
-    r = GRIN.get_station_id_by_name('St. Jacobs')
+    r = GRIN.station_id_by_name('St. Jacobs')
 		expect(r).to eq("14475")
   end
 
 	it 'gets a timeseries for a given name' do
-		r = GRIN.get_timeseries_list('St. Jacobs')
+		# r = GRIN.get_timeseries_list('8665042', '2016-11-09T09:00:00', '2016-11-09T10:00:00')
+		r = GRIN.timeseries_list('8671042', '2016-11-09T09:00:00', '2016-11-09T10:00:00')
 		expect(r).to be_a(Array)
 	end
+
+  it 'returns a list of possible parameters' do
+    r = GRIN.parameter_list(14475)
+    expect(r).to be_a(Array)
+  end
+
+  it 'returns the waterflow for a station on a given day' do
+    r = GRIN.waterflow(:westmontrose, '2016-10-29')
+    expect(r).to be_a(Numeric)
+  end
+
+  it 'returns nil for an unknown station' do
+    r = GRIN.waterflow(:unknown, '2016-10-29')
+    expect(r).to be_nil
+  end
+
+  it 'return to relative flow to normal Summer low flow on a certain day' do
+    r = GRIN.waterflow(:westmontrose, '2016-10-29')
+    expect(r).to be < 5.0
+  end
+
+  it 'return to relative flow to normal Summer low flow today' do
+    r = GRIN.waterflow(:westmontrose)
+    expect(r).to be_a(Numeric)
+  end
+
 end
