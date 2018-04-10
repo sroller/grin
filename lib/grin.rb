a7b600094b3c87d95774b063890078006e42169d
Dr. Steffen Roller <steffen.roller@gmail.com>
2018-04-10 17:37:31 -0400

    add St. Jacobs

--- lib/grin.rb
+++ lib/grin.rb
@@ -13,7 +13,8 @@ Waterflow = {
   victoria: '8899042',
   doon: '8677042',
   galt: '8671042',
-  newhamburg: '8827042'
+  newhamburg: '8827042',
+	stjacobs: '8641042'
 }
 
 Summerflow = {
@@ -21,7 +22,8 @@ Summerflow = {
   bridgeport: 11.0,
   doon: 11.0,
   galt: 15,
-  newhamburg: 1.5
+  newhamburg: 1.5,
+	stjacobs: 4.0
 }
 
 
@@ -40,14 +42,14 @@ BASE_URL = 'http://kiwis.grandriver.ca/KiWIS/KiWIS?service=kisters&type=querySer
 
   def self.station_by_name(name='*')
     clnt = HTTPClient.new
-    r = clnt.get(BASE_URL+"getStationList&station_name=#{name}")
+		r = clnt.get(BASE_URL+"getStationList&station_name=#{URI.escape(name)}")
     { status: r.status, content: JSON.parse((r.body))}
   end
 
 	def self.station_id_by_name(name)
 		return nil if name.nil?
     clnt = HTTPClient.new
-    r = clnt.get(BASE_URL+"getStationList&station_name=#{name}")
+		r = clnt.get(BASE_URL+"getStationList&station_name=#{URI.escape(name)}")
 		JSON.parse(r.body)[1][2]
 	end
 
@@ -69,13 +71,19 @@ BASE_URL = 'http://kiwis.grandriver.ca/KiWIS/KiWIS?service=kisters&type=querySer
 
   def self.waterflow(station, day=nil)
     return nil unless Waterflow.has_key? station
-    day = Time.now if day.nil?
+		unless day.nil?
+			day = DateTime.parse(day)
+		else
+			day = DateTime.now
+		end
     url = BASE_URL+"gettimeseriesvalues&ts_id=#{Waterflow[station]}"
-    url += "&from=#{(day-7200).strftime("%Y-%m-%dT%H:%M:%SZ")}&to=#{day.strftime("%Y-%m-%dT%H:%M:%SZ")}"
+		# fetch the last 24 hours
+    url += "&from=#{(day-1).strftime("%Y-%m-%dT%H:%M:%SZ")}&to=#{day.strftime("%Y-%m-%dT%H:%M:%SZ")}"
     clnt = HTTPClient.new
     # ap url
     begin
         r = clnt.get(url)
+				# ap r
     rescue StandardError => e
         puts "ERROR: #{e}"
         exit
@@ -87,8 +95,12 @@ BASE_URL = 'http://kiwis.grandriver.ca/KiWIS/KiWIS?service=kisters&type=querySer
         puts "ERROR: JSON parse error"
         ap url
     else
-        avg = data.inject(0.0) { |sum,item| sum += item[1] } / data.size
-        data.inject(0.0) { |sum,item| sum += item[1] } / data.size
+			# ap data
+			# print "%.2f/%.2f/%.2f" % [data[-1][1], data[-2][1], data[0][1]]
+			# "%.2f/%.2f/%.2f" % [data[-1][1], data[-2][1], data[0][1]]
+      # avg = data.inject(0.0) { |sum,item| sum += item[1] } / data.size
+      # data.inject(0.0) { |sum,item| sum += item[1] } / data.size
+			data[-1][1]
     end
   end
 
