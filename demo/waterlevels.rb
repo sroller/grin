ff05f7bf02a476eceb87ee12504a97a009f7f083
Dr. Steffen Roller <steffen.roller@gmail.com>
2018-04-23 00:16:11 -0400

    query includes explicit Time value

--- demo/waterlevels.rb
+++ demo/waterlevels.rb
@@ -4,14 +4,15 @@ $LOAD_PATH << File.dirname(__FILE__)+ '/../lib'
 
 require_relative '../lib/grin'
 
-[:stjacobs, :westmontrose, :bridgeport, :doon, :galt].each do |station|
-    flow = GRIN.waterflow(station)
+[:stjacobs, :westmontrose, :bridgeport, :victoria, :doon, :galt].each do |station|
+  datum = Time.now.to_s
+  flow = GRIN.waterflow(station, datum)
     # ap flow
     unless flow.nil?
-      puts "#{Time.now.strftime("%Y-%m-%d %H:%M")}:%-14s %6.2f cbm/s (%d%% of Summer low)" % [ station.to_s.capitalize, flow, GRIN.relative_flow(station)*100 ]
+      puts "#{Time.now.strftime("%Y-%m-%d %H:%M")}:%-14s %6.2f cbm/s (%d%% of Summer low)" % [ station.to_s.capitalize, flow, GRIN.relative_flow(station, datum)*100 ]
         # print station.to_s.capitalize, " %s (now/60min/24h)" % flow, " (", "%d%% Summer low)\n" % (GRIN.relative_flow(station)*100)
     else
-        print "problem with station ", station
+        puts "problem with station #{station}"
     end
 end
 
